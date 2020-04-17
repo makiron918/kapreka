c13d5a7ca5c582837d979ed0d7d75a7d60b5324d
makiron918 <gonzaress918@gmail.com>
2020-04-17 09:40:46 +0900

    Update kapreka.rb

--- kapreka.rb
+++ kapreka.rb
@@ -1,3 +1,11 @@
-input = get.to_s
+def kapreka(n, result)
+  max = n.to_s.split(//).sort.reverse.to_s.to_i
+  min = n.to_s.splot(//).sort.to_s.to_i
+  kap = max - min
+  if result.member?(kap)
+    return results.push(kap)
+  else
+    return kaprekar2(kap, result.push(kap))
+  end
+end
 
-input.split
