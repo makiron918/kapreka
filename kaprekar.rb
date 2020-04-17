27f6755d652145bfacdffaad15628d838bc75f02
makiron918 <gonzaress918@gmail.com>
2020-04-17 23:22:52 +0900

    Update kaprekar.rb

--- kaprekar.rb
+++ kaprekar.rb
@@ -4,15 +4,19 @@
 
 def input_num
   input = gets.to_s
-  # kap = []
-  # kap << input
   min = input.to_s.split("").sort.join.to_i
   max = input.to_s.split("").sort.reverse.join.to_i
-  if max - min > input.to_i
+  diff = max - min
+  result = "%0#{input.length-1}d" % diff
+
+  while result != input do
+    input = "%0#{input.length-1}d" % diff
+    min = input.to_s.split("").sort.join.to_i
+    max = input.to_s.split("").sort.reverse.join.to_i
     diff = max - min
-  else
-    return diff
+    result = "%0#{input.length-1}d" % diff
   end
+  puts result
 end
 
 # min = 152.to_s.split("").sort.join.to_i
