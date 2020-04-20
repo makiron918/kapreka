bfdd336cfea67c735369fd691f8b3b5d5eab399a
makiron918 <gonzaress918@gmail.com>
2020-04-20 09:53:46 +0900

    Update kaprekar.rb

--- kaprekar.rb
+++ kaprekar.rb
@@ -3,23 +3,24 @@ def input_num
   input = gets.to_s
   if input.to_i > 100000000000000 || input.to_i < 0
     puts "入力値を0以上100,000,000,000,000以下にしてください"
-  end
-  min = input.to_s.split("").sort.join.to_i
-  max = input.to_s.split("").sort.reverse.join.to_i
-  diff = max - min
-  result = "%0#{input.length-1}d" % diff
-
-  for i in 1..100 do
-    input = "%0#{input.length-1}d" % diff
+  else
     min = input.to_s.split("").sort.join.to_i
     max = input.to_s.split("").sort.reverse.join.to_i
     diff = max - min
     result = "%0#{input.length-1}d" % diff
-  end
-  if result != input
-    puts "カプレカ数は見つかりませんでした"
-  else
-    puts result
+
+    for i in 1..100 do
+      input = "%0#{input.length-1}d" % diff
+      min = input.to_s.split("").sort.join.to_i
+      max = input.to_s.split("").sort.reverse.join.to_i
+      diff = max - min
+      result = "%0#{input.length-1}d" % diff
+    end
+    if result != input
+      puts "カプレカ数は見つかりませんでした"
+    else
+      puts result
+    end
   end
 end
 
