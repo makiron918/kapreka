49df5459086e864955f3bb5e67ffb8a7f6a1decf
makiron918 <gonzaress918@gmail.com>
2020-04-21 09:39:20 +0900

    Update kapreka.rb

--- kapreka.rb
+++ kapreka.rb
@@ -13,5 +13,21 @@ def kapreka(n)
   kapreka_re(num, [])
 end
 
-kapreka(156)
+def is_kaprekar_number(num)
+  characters = num.to_s.split('').sort
+  min = characters.join.to_i
+  max = characters.reverse.join.to_i
+  return (max - min) == num
+end
+def next_kaprekar_number(start)
+  # カプレカ数は9の倍数しかないので、9の倍数から始める
+  num = ((start + 8) / 9) * 9
+  loop do
+    return num if is_kaprekar_number(num)
+    num = num + 9
+  end
+end
+
+input = STDIN.gets.chomp
+puts next_kaprekar_number(input.to_i)
 
