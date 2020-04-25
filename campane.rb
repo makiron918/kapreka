3d4a3afd2f860b987fd845cf2cd86a1d794043c4
makiron918 <gonzaress918@gmail.com>
2020-04-25 10:32:47 +0900

    Update campane.rb

--- campane.rb
+++ campane.rb
@@ -6,35 +6,44 @@ k = int(input_str[1])
 # a_1, a_2 ... a_n を取得
 input_list = input().split()
 a_list = []
-for a_i in input_list:
+for a_i in input_list do
     a_list.append(int(a_i))
+end
 
 # キャンペーンの各区間の平均値
 average = []
 
-for i in range(n - k + 1):
-    i_average = 0
-    for j in range(k):  # k日間分足して平均を求める
-        i_average += a_list[i + j]
+for i in range(n - k + 1) do
+  i_average = 0
+  for j in range(k) do  # k日間分足して平均を求める
+      i_average += a_list[i + j]
 
-    average.append(i_average / k)
+  average.append(i_average / k)
+  end
+end
 
 
 ans_max = 0
-for i in average:  # 最大値をループで探す
-    if ans_max < i:
-        ans_max = i
+for i in average do  # 最大値をループで探す
+  if ans_max < i
+      ans_max = i
+  end
+end
 
 
 ans_first_max = 0
-for i in average:  # 最初に出てくる日を探す
-    if i == ans_max:
-        break
-    ans_first_max += 1
+for i in average do  # 最初に出てくる日を探す
+  if i == ans_max
+      break
+  ans_first_max += 1
+  end
+end
 
 ans_count = 0
-for i in average:  # 最大値と同じ値の個数を求める
-    if ans_max == i:
-        ans_count += 1
+for i in average do  # 最大値と同じ値の個数を求める
+  if ans_max == i
+      ans_count += 1
+  end
+end
 
-print(str(ans_count) + " " + str(ans_first_max + 1))
+puts str(ans_count) + " " + str(ans_first_max + 1)
