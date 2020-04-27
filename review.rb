b9b521180ae26f839160f132568164f8035304ef
makiron918 <gonzaress918@gmail.com>
2020-04-27 16:40:21 +0900

    Update review.rb

--- review.rb
+++ review.rb
@@ -1,12 +1,11 @@
-genre = "映画"
-title ="時をかける少女"
-review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\nTime waits for no one."
+puts "ジャンルを入力してください："
+genre = gets.chomp
+puts "タイトルを入力してください："
+title = gets.chomp
+puts "感想を入力してください："
+review = gets.chomp
 line = "---------------------------"
 
 puts "ジャンル：#{genre}\n#{line}"
 puts "タイトル：#{title}\n#{line}"
 puts "感想：\n#{review}\n#{line}"
-
-genre = gets.chomp
-title = gets.chomp
-review = gets.chomp
