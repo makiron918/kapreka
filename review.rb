4d8a78aca2165b1dbe73cf726213d0fb55a0219b
makiron918 <gonzaress918@gmail.com>
2020-04-27 17:52:33 +0900

    Update review.rb

--- review.rb
+++ review.rb
@@ -1,11 +1,13 @@
+post = {}
+
 puts "ジャンルを入力してください："
-genre = gets.chomp
+post[:genre] = gets.chomp
 puts "タイトルを入力してください："
-title = gets.chomp
+post[:title] = gets.chomp
 puts "感想を入力してください："
-review = gets.chomp
+post[:review] = gets.chomp
 line = "---------------------------"
 
-puts "ジャンル：#{genre}\n#{line}"
-puts "タイトル：#{title}\n#{line}"
-puts "感想：\n#{review}\n#{line}"
+puts "ジャンル：#{post[:genre]}\n#{line}"
+puts "タイトル：#{post[:title]}\n#{line}"
+puts "感想：\n#{post[:review]}\n#{line}"
