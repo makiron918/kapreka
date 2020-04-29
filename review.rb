cd9ad89d22b2d2582c7f78bdd5bcb7cb6c6a201e
makiron918 <gonzaress918@gmail.com>
2020-04-29 11:49:17 +0900

    Update review.rb

--- review.rb
+++ review.rb
@@ -1,23 +1,29 @@
-
-
 puts "レビュー数：0"
 puts "[0]レビューを書く"
 puts "[1]レビューを読む"
 puts "[2]アプリを終了する"
 input = gets.to_i
 
+if input == 0 
+  post = {}
+
+  puts "ジャンルを入力してください："
+  post[:genre] = gets.chomp
+  puts "タイトルを入力してください："
+  post[:title] = gets.chomp
+  puts "感想を入力してください："
+  post[:review] = gets.chomp
+  line = "---------------------------"
+
+  puts "ジャンル：#{post[:genre]}\n#{line}"
+  puts "タイトル：#{post[:title]}\n#{line}"
+  puts "感想：\n#{post[:review]}\n#{line}"
+elsif input == 1
+  # レビューを読む
+elsif input == 2
+  # プログラムを終了
+else
+  puts "無効な値です"
+end
 
 
-post = {}
-
-puts "ジャンルを入力してください："
-post[:genre] = gets.chomp
-puts "タイトルを入力してください："
-post[:title] = gets.chomp
-puts "感想を入力してください："
-post[:review] = gets.chomp
-line = "---------------------------"
-
-puts "ジャンル：#{post[:genre]}\n#{line}"
-puts "タイトル：#{post[:title]}\n#{line}"
-puts "感想：\n#{post[:review]}\n#{line}"
