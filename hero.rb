281abd3d75edf43c08f66ddff35e179a62f588a3
makiron918 <gonzaress918@gmail.com>
2020-05-02 13:34:32 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -1,37 +1,68 @@
-def register_data
-
+def register_data(data)
+hero = {}
   # 名前、パンチ力、キック力、ジャンプ力をユーザーに入力させ、保存する。
   puts "名前を入力してください"
+  hero[:name] = gets.chomp
   puts "パンチ力を入力してください(0 ~ 100)"
+  hero[:punch] = gets.to_i
   puts "キック力を入力してください(0 ~ 100)"
+  hero[:kick] = gets.to_i
   puts "ジャンプ力を入力してください(0 ~ 100)"
+  hero[:jump] = gets.to_i
+
+  data << hero
 
 end
 
 
-def power
+def power(punch, kick, jump)
 
   # 入力された数字が0~100になるまで繰り返し入力させる。
-  puts "100以下の数字を入力してください"
+  while hero[:punch] <= 100 && hero[:kick] <= 100 && hero[:jump] <= 100 do 
+    puts "100以下の数字を入力してください"
+  end
 
 end
 
 
 def hero_rank(punch, kick, jump)
 
-  # ヒーロランクの判定をする。
+  rank = hero[:punch] + hero[:kick] + hero[:jump]
+  if rank == 300
+    hero[:rank] = A
+  elsif 100 <= rank && rank < 300
+    hero[:rank] = B
+  elsif 20 <= rank && rank < 100
+    hero[:rank] = C
+  else
+    hero[:rank] = D
+  end
 
 end
 
 
 def show_data_list(data)
-
-   puts "見たい番号を入力してください"
+  index = 1
   # 保存されたデータの一覧を出力する。
+  data.each do |hero|
+    puts "#{index}:#{hero[:name]}"
+    index += 1
+  end
+  puts "見たい番号を入力してください"
+  input = gets.to_i
+  if input = index
+    puts "名前：#{hero[:name]}"
+    puts "パンチ力：#{hero[:punch]}"
+    puts "キック力：#{hero[:kick]}"
+    puts "ジャンプ力：#{hero[jump]}"
+    puts "ヒーローランク：#{hero[:rank]}"
+  end
 
   # 選択されたデータの詳細な情報（名前、パンチ力、キック力、ジャンプ力、ヒーロランク）を出力する。
 
 end
+
+data = []
 
 while true
   puts '選択してください'
@@ -41,9 +72,9 @@ while true
   input = gets.to_i
 
   if input == 0
-    # データの登録を行う。
+    register_data(data)
   elsif input == 1
-    # 保存されたデータの一覧を表示する。
+    show_data_list(data)
   elsif input == 2
     exit
   else
