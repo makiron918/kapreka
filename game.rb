5e4c93b69805cb9eee087a7d04f18dfff5869f2f
makiron918 <gonzaress918@gmail.com>
2020-05-09 22:25:29 +0900

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,6 +1,11 @@
 def register game
   game = {}
   puts "ゲームのジャンルを入力してください："
+  game[:genre] = gets.chomp
   puts "ゲームのハード名を入力してください："
+  game[:platform] = gets.chomp
   puts "ゲームのタイトルを入力してください："
+  game[:title] = gets.chomp
+
+  games << game
 end
