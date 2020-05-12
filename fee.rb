47834c898dee75a993f1b9b6fee2b64a4733848a
makiron918 <gonzaress918@gmail.com>
2020-05-12 21:40:31 +0900

    Update fee.rb

--- fee.rb
+++ fee.rb
@@ -1,5 +1,16 @@
 card = gets.split
 points = 0
+money = card[0].to_i
 # 一つ目が運賃、二つ目が乗車回数
 (card[1].to_i).times do
-  
+  fee = gets.to_i
+  point = fee * 0.1
+  if points >= fee
+    points = points - fee
+    card[0].to_i += 0
+  else
+    money = money - fee
+    points += point
+  end
+  puts money, point
+end
