bdb1473e2c32d0e162f82abfb1aaa05ad9d108d3
makiron918 <gonzaress918@gmail.com>
2021-04-10 10:01:05 +0900

    Update hensu.py

--- python/hensu.py
+++ python/hensu.py
@@ -1,13 +1,17 @@
-num = 1
-num01 = 2
-num_01 = 3
-# num$01 = 4
-# num-01 = 5
-# 01num = 6
+# num = 1
+# num01 = 2
+# num_01 = 3
+# # num$01 = 4
+# # num-01 = 5
+# # 01num = 6
+# num01 = 123
+# num02 = 1.23
 
-print(num)
-print(num01)
-print(num_01)
-# print(num$01)
-# print(num-01)
-# print(01num)
+# print(num)
+# print(num01)
+# print(num_01)
+# # print(num$01)
+# # print(num-01)
+# # print(01num)
+# print(type(num01))
+# print(type(num02))
