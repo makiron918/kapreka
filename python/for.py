08af1231878fda6996beba0309fb7da5cfc01d04
makiron918 <gonzaress918@gmail.com>
2021-04-10 10:39:27 +0900

    Update for.py

--- python/for.py
+++ python/for.py
@@ -2,8 +2,26 @@
 #   for j in range(3):
 #     print(i, j)
 
-arr = [2, 4, 6, 8, 10]
-sum = 0
-for i in arr:
-  sum += i
-print(sum)
+# arr = [2, 4, 6, 8, 10]
+# sum = 0
+# for i in arr:
+#   sum += i
+# print(sum)
+
+# def say_hello(greeting):
+#   print(greeting)
+
+# hello = say_hello
+# hello("Good Morning")
+
+# def add(num01, num02):
+#   return(num01 + num02)
+
+# add_result = add(6, 2)
+# print(add_result)
+
+def average(num01, num02, num03):
+  return((num1 + num02 + num03)/3)
+
+average_result = average(9, 4, 2)
+print(average_result)
