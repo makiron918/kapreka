d3bd982ba972716aba36f1a292f7a06fb75395d8
makiron918 <gonzaress918@gmail.com>
2021-04-18 09:06:24 +0900

    Update class.py

--- python/class.py
+++ python/class.py
@@ -1,6 +1,13 @@
 class Student:
+
+  def __init__(self,name):
+    self.name = name
+
   def avg(self,math,english):
     print((math + english)/ 2)
 
-a001 = Student()
-a001.avg(30,70)
+a001 = Student("sato")
+print(a001.name)
+
+a002 = Student("tanaka")
+print(a002.name)
